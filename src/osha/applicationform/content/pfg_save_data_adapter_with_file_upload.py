e2eddcf8b5e7c5eb0bca9e0c5b494fcdb7deb725
Peter Lamut <peter.lamut@niteoweb.com>
2012-12-17 13:53:41 +0100

    Save uploded files to a folder, add application UUID to CSV fields

--- src/osha/applicationform/content/pfg_save_data_adapter_with_file_upload.py
+++ src/osha/applicationform/content/pfg_save_data_adapter_with_file_upload.py
@@ -4,12 +4,14 @@
 from AccessControl import ClassSecurityInfo
 from osha.applicationform.config import PROJECTNAME
 from osha.applicationform.interfaces import IPFGSaveDataAdapterWithFileUpload
+from plone import api as plone_api
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import schemata
 from Products.CMFCore.permissions import View
 from Products.PloneFormGen.content.saveDataAdapter import FormSaveDataAdapter
 from zope.interface import implements
 
+import uuid
 
 PFGSaveDataAdapterWithFileUploadSchema = FormSaveDataAdapter.schema.copy() + atapi.Schema((
     # -*- Your Archetypes field definitions here ... -*-
@@ -42,17 +44,68 @@ class PFGSaveDataAdapterWithFileUpload(FormSaveDataAdapter):
     def onSuccess(self, fields, REQUEST=None):
         """The essential method of a PloneFormGen Adapter."""
 
+        # 1. v csv zapises nek ID ki ga dobis iz plone.uuid
+        #     to naredis tako, da dolocis nek uuid in ga dodas v fields
+        #      potem naj se zadeva shrani ...
+        application_uuid = str(uuid.uuid4())  # uuid4 = random UUID
+
+        REQUEST.form['application_uuid'] = application_uuid
+
+
+        # # TODO: dodaj application_uuid?
+        # atapi.StringField('application_uuid',
+        #     required = True,
+        #     default = uuid.uuid4(),  # uuid4 = random UUID
+        # ),
+
+        # TODO: nekje treba shemo dodati .. tole dodatno polje ...
+        # fields = [<FGStringField at /Plone/my-form-folder/first-name>]
+
+        # fields[0].fgField.get(REQUEST) ... tako dobis vrednost
+
+        # ocitno treba v seznam polj dodati uuid pa to v request podturiti,
+        # da bo notri in se bo shranilo v CSV
+
+        # REQUEST.form.get("fieldName", "default_val_if_missing")
+
+
+
+        folder = plone_api.content.get(UID=application_uuid)
+        if not folder:
+            form_folder = plone_api.content.get(path=REQUEST["PATH_INFO"])  # TODO: this OK?
+            folder = plone_api.content.create(
+                container=form_folder,
+                type="Folder",
+                id=application_uuid,
+            )
+
+        # notr v folder kreiras za vsak uploadan file en ATFile
+        # save CV and motivation letter
+        plone_api.content.create(
+            container=folder,
+            type="File",
+            id="CV",
+            file=REQUEST.form['cv_file']
+            # TODO: file contents ... cv_file
+        )
+
+        plone_api.content.create(
+            container=folder,
+            type="File",
+            id="motivation-letter",
+            file=REQUEST.form['motivation-letter_file']
+        )
+
+        # NOTE: this has to be called *after* we save files, because otherwise
+        # uploaded file contents are not available anymore in REQUEST
         super(PFGSaveDataAdapterWithFileUpload, self).onSuccess(fields, REQUEST)
 
-        print("This is overriden onSucces method")
+        # popravek 2.: v folder uploads nov subfolder z idjem istim kot si ga zapisal
+        # v CSV in potem v ta subfolder das ATFile
 
-        # api.content.create()
-        # pogledas ce folder "uploads" obstaja in ce ne, kreiras
-        # notr v folder kreiras za vsak uploadan file en ATFile
-        # aha
-        # hm, no bom probal nekaj spacat :)
-        # 3. v csv zapises nek ID ki ga dobis iz plone.uuid -> oz. tole more bit prvi korak
-        # popravek 2.: v folder uploads nov subfolder z idjem istim kot si ga zapisal v CSV in potem v ta subfolder das ATFile
+
+
+        # TODO: on error transaction abort?
 
 
 atapi.registerType(PFGSaveDataAdapterWithFileUpload, PROJECTNAME)

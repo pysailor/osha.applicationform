98e5b2be211dfeaec2075100b5239a7bfa088490
jcerjak <jcerjak@termitnjak.si>
2013-02-12 14:16:58 +0100

    add a view for save data adapter
    It displays saved data with links to uploaded files and option to export the results.

--- src/osha/applicationform/browser/views.py
+++ src/osha/applicationform/browser/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from DateTime import DateTime
 from osha.applicationform.config import NATIONALITIES
+from osha.applicationform.config import PFG_FILE_UPLOAD_PREFIX
 from plone.i18n.locales.countries import countries
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
@@ -33,3 +34,24 @@ class NationalitiesView(BrowserView):
 
     def __call__(self):
         return [(item.lower(), item) for item in NATIONALITIES]
+
+
+class PFGSaveDataAdapterWithFileUploadView(BrowserView):
+    """ View for displaying saved data (with links to uploaded files) and
+    option to export the results.
+
+    INFO: Template is a combination of fg_savedata_view_p3.pt and
+    fg_savedata_tabview_p3.pt from Products.PloneFormGen.
+    """
+
+    def __call__(self):
+        return self.index()
+
+    def get_file_url(self, file_id):
+        """ Return url to the uploaded file. We need the file prefix to
+        identify the file in the results row.
+        """
+        return "%s/%s/application/view" % (
+            self.context.getParentNode()['uploads'].absolute_url(),
+            file_id.strip(PFG_FILE_UPLOAD_PREFIX + '-')
+        )
